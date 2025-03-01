fb38ff84bdea1a3d6c5a15cc9aef32ad00eaceae
Raissa Santos22 <raissa.santos171@etec.sp.gov.br>
2025-03-01 10:12:33 -0300

    Aula de conversão de tipos, e primeiro programa

---
+++ src/ConversaoDeTipos.java
@@ -0,0 +1,22 @@
+import javax.swing.text.FlowView;
+public class ConversaoDeTipos {
+    public static void main(String[] arg) {
+        String s1 = "10";
+
+        int v = Integer.parseInt(s1);
+
+        float x = Float.parseFloat(s1);
+
+        double y = Double.parseDouble(s1);
+
+        int w = (int) x;
+
+        int z = (int) y;
+
+        String s2 = String.valueOf(v);
+
+
+        System.out.println(s2 + w + z);
+
+    }
+}
---
+++ src/exec01.java
@@ -0,0 +1,13 @@
+public class exec01 {
+    public static void main(String[] arg) {
+        String nome = "Maria Eduarda";
+        int idade = 16;
+        float altura = 1.62f;
+        int cpf = 1234567890;
+
+        System.out.println("Nome : " + nome);
+        System.out.println("Idade : " + idade);
+        System.out.println("Altura : " + altura);
+        System.out.println("CPF : " + cpf);
+    }
+}
